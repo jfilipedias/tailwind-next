5ab85f608d5fbb3d91d4738853f0aac03da76a88
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-10-07 09:40:45 -0300

    feat: Starts to implement the FileList

--- src/components/form/file-input.tsx
+++ src/components/form/file-input.tsx
@@ -10,6 +10,7 @@ import {
 	useState,
 } from 'react'
 import { UploadCloud, User } from 'lucide-react'
+import { formatBytes } from '@/utils/format-bytes'
 
 interface FileInputContextType {
 	id: string
@@ -105,5 +106,35 @@ export function ImagePreview() {
 			alt=""
 			className="h-16 w-16 rounded-full object-cover"
 		/>
+	)
+}
+
+export function FileList() {
+	const { files } = useFileInput()
+
+	return (
+		<div className="mt-4 space-y-3">
+			{files.map((file) => (
+				<div
+					key={file.name}
+					className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4"
+				>
+					<div className="rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600">
+						<UploadCloud className="h-4 w-4" />
+					</div>
+
+					<div className="flex flex-1 flex-col items-start gap-1">
+						<div className="flex flex-col">
+							<span className="to-zinc-700 text-sm font-medium">
+								{file.name}
+							</span>
+							<span className="text-sm text-zinc-500">
+								{formatBytes(file.size)}
+							</span>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
 	)
 }
