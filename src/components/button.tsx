c68222bb28b91610b9111eec26584419edb0adff
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-10-12 19:04:27 -0300

    refactor: Change cva to tailwind-variant

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,25 +1,23 @@
 import { ComponentProps } from 'react'
-import { VariantProps, cva } from 'class-variance-authority'
+import { VariantProps, tv } from 'tailwind-variants'
 
-const buttonStyles = cva(
-	[
+const buttonStyles = tv({
+	base: [
 		'rounded-lg px-4 py-2 text-sm font-semibold outline-none shadow-sm',
 		'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500',
 		'active:opacity-80',
 	],
-	{
-		variants: {
-			variant: {
-				primary: 'bg-violet-600 text-white hover:bg-violet-700',
-				outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
-				ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none',
-			},
-		},
-		defaultVariants: {
-			variant: 'primary',
+	variants: {
+		variant: {
+			primary: 'bg-violet-600 text-white hover:bg-violet-700',
+			outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
+			ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none',
 		},
 	},
-)
+	defaultVariants: {
+		variant: 'primary',
+	},
+})
 
 type ButtonStylesProps = VariantProps<typeof buttonStyles>
 
