b5c422edd3299322cc966a425c45e43bccba458b
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-10-12 19:04:00 -0300

    feat: Create the FileItem component

--- src/components/form/file-input.tsx
+++ src/components/form/file-input.tsx
@@ -9,7 +9,7 @@ import {
 	useMemo,
 	useState,
 } from 'react'
-import { Trash2, UploadCloud, User } from 'lucide-react'
+import { CheckCircle2, Trash2, UploadCloud, User } from 'lucide-react'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { Button } from '@/components/button'
 import { formatBytes } from '@/utils/format-bytes'
@@ -115,6 +115,68 @@ export function ImagePreview() {
 	)
 }
 
+interface FileItemProps {
+	name: string
+	size: number
+}
+
+function FileItem({ name, size }: FileItemProps) {
+	const state = 'error' as 'progress' | 'error' | 'completed'
+
+	return (
+		<div className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4">
+			<div className="rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600">
+				<UploadCloud className="h-4 w-4" />
+			</div>
+
+			{state === 'error' ? (
+				<div className="flex flex-1 flex-col items-start gap-1">
+					<div className="flex flex-col">
+						<span className="text-error-700 text-sm font-medium">
+							Upload failed, please try again.
+						</span>
+						<span className="text-error-600 text-sm">{name}</span>
+					</div>
+
+					<button
+						type="button"
+						className="text-error-700 hover:text-error-900 text-sm font-semibold"
+					>
+						Try again
+					</button>
+				</div>
+			) : (
+				<div className="flex flex-1 flex-col items-start gap-1">
+					<div className="flex flex-col">
+						<span className="text-sm font-medium text-zinc-700">{name}</span>
+						<span className="text-sm text-zinc-500">{formatBytes(size)}</span>
+					</div>
+
+					<div className="flex w-full items-start gap-3">
+						<div className="h-2 flex-1 rounded-full bg-zinc-100">
+							<div
+								className="h-2 rounded-full bg-violet-600"
+								style={{ width: state === 'completed' ? '100%' : '80%' }}
+							/>
+						</div>
+						<span className="text-sm font-medium text-zinc-700">
+							{state === 'completed' ? '100%' : '80%'}
+						</span>
+					</div>
+				</div>
+			)}
+
+			{state === 'completed' ? (
+				<CheckCircle2 className="h-5 w-5 fill-violet-600 text-white" />
+			) : (
+				<Button variant="ghost" type="button" title="Remove">
+					<Trash2 className="h-5 w-5 text-zinc-500" />
+				</Button>
+			)}
+		</div>
+	)
+}
+
 export function FileList() {
 	const { files } = useFileInput()
 	const [parent] = useAutoAnimate()
@@ -122,36 +184,7 @@ export function FileList() {
 	return (
 		<div ref={parent} className="mt-4 space-y-3">
 			{files.map((file) => (
-				<div
-					key={file.name}
-					className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4"
-				>
-					<div className="rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600">
-						<UploadCloud className="h-4 w-4" />
-					</div>
-
-					<div className="flex flex-1 flex-col items-start gap-1">
-						<div className="flex flex-col">
-							<span className="to-zinc-700 text-sm font-medium">
-								{file.name}
-							</span>
-							<span className="text-sm text-zinc-500">
-								{formatBytes(file.size)}
-							</span>
-						</div>
-
-						<div className="flex w-full items-start gap-3">
-							<div className="h-2 flex-1 rounded-full bg-zinc-100">
-								<div className="h-2 w-4/5 rounded-full bg-violet-600" />
-							</div>
-							<span className="text-sm font-medium text-zinc-700">80%</span>
-						</div>
-					</div>
-
-					<Button variant="ghost" type="button" title="Remove">
-						<Trash2 className="h-5 w-5 text-zinc-500" />
-					</Button>
-				</div>
+				<FileItem key={file.name} name={file.name} size={file.size} />
 			))}
 		</div>
 	)
