168d97a9de40939efb6f31e15326be03d47fbd84
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-10-18 22:20:10 -0300

    feat: Finish dark mode theme

--- src/components/form/file-input.tsx
+++ src/components/form/file-input.tsx
@@ -45,16 +45,18 @@ export function Trigger({ children, ...props }: TriggerProps) {
 	return (
 		<label
 			{...props}
-			className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
+			className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500 dark:border-zinc-800 dark:hover:border-zinc-700 dark:hover:bg-zinc-800 dark:hover:text-violet-300"
 			htmlFor={id}
 		>
-			<div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
-				<UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+			<div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100 dark:border-zinc-700 dark:bg-zinc-800 dark:group-hover:border-zinc-600 dark:group-hover:bg-zinc-700">
+				<UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600 dark:text-zinc-500 dark:group-hover:text-violet-300" />
 			</div>
 
 			<div className="flex flex-col items-center">
 				<span className="text-sm">
-					<span className="font-semibold text-violet-700">Click to upload</span>{' '}
+					<span className="font-semibold text-violet-700 dark:text-violet-300">
+						Click to upload
+					</span>{' '}
 					or drag and drop
 					{children}
 				</span>
@@ -101,8 +103,8 @@ export function ImagePreview() {
 
 	if (previewURL === null) {
 		return (
-			<div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
-				<User className="h-8 w-8 text-violet-500" />
+			<div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50 dark:bg-violet-500/10">
+				<User className="h-8 w-8 text-violet-500 dark:text-violet-300" />
 			</div>
 		)
 	}
@@ -119,21 +121,22 @@ export function ImagePreview() {
 const fileItemStyles = tv({
 	slots: {
 		root: 'group flex items-start gap-4 rounded-lg border p-4',
-		icon: 'rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600',
+		icon: 'rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-500',
 		deleteButton: 'text-zinc-500',
 	},
 	variants: {
 		state: {
 			progress: {
-				root: 'border-zinc-200',
+				root: 'border-zinc-200 dark:border-zinc-700',
 			},
 			complete: {
-				root: 'border-violet-500',
+				root: 'border-violet-500 dark:border-violet-300/40',
 			},
 			error: {
-				root: 'bg-error-25 border-error-300',
-				icon: 'border-error-50 bg-error-100 text-error-600',
-				deleteButton: 'text-error-700 hover:text-error-900',
+				root: 'bg-error-25 border-error-300 dark:bg-error-500/5 dark:border-error-500/30',
+				icon: 'border-error-50 bg-error-100 text-error-600 dark:bg-error-500/5 dark:border-error-500/30 dark:text-error-400',
+				deleteButton:
+					'text-error-700 hover:text-error-900 dark:text-error-400 dark:hover:text-error-300',
 			},
 		},
 	},
@@ -159,15 +162,17 @@ function FileItem({ name, size, state }: FileItemProps) {
 			{state === 'error' ? (
 				<div className="flex flex-1 flex-col items-start gap-1">
 					<div className="flex flex-col">
-						<span className="text-error-700 text-sm font-medium">
+						<span className="text-sm font-medium text-error-700 dark:text-error-500">
 							Upload failed, please try again.
 						</span>
-						<span className="text-error-600 text-sm">{name}</span>
+						<span className="text-sm text-error-600 dark:text-error-400">
+							{name}
+						</span>
 					</div>
 
 					<button
 						type="button"
-						className="text-error-700 hover:text-error-900 text-sm font-semibold"
+						className="text-sm font-semibold text-error-700 hover:text-error-900 dark:text-error-400 dark:hover:text-error-300"
 					>
 						Try again
 					</button>
@@ -175,18 +180,22 @@ function FileItem({ name, size, state }: FileItemProps) {
 			) : (
 				<div className="flex flex-1 flex-col items-start gap-1">
 					<div className="flex flex-col">
-						<span className="text-sm font-medium text-zinc-700">{name}</span>
-						<span className="text-sm text-zinc-500">{formatBytes(size)}</span>
+						<span className="text-sm font-medium text-zinc-700 dark:text-zinc-100">
+							{name}
+						</span>
+						<span className="text-sm text-zinc-500 dark:text-zinc-400">
+							{formatBytes(size)}
+						</span>
 					</div>
 
-					<div className="flex w-full items-start gap-3">
-						<div className="h-2 flex-1 rounded-full bg-zinc-100">
+					<div className="flex w-full items-center gap-3">
+						<div className="h-2 flex-1 rounded-full bg-zinc-100 dark:bg-zinc-600">
 							<div
-								className="h-2 rounded-full bg-violet-600"
+								className="h-2 rounded-full bg-violet-600 dark:bg-violet-400"
 								style={{ width: state === 'complete' ? '100%' : '80%' }}
 							/>
 						</div>
-						<span className="text-sm font-medium text-zinc-700">
+						<span className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
 							{state === 'complete' ? '100%' : '80%'}
 						</span>
 					</div>
@@ -220,7 +229,7 @@ export function FileList() {
 					key={file.name}
 					name={file.name}
 					size={file.size}
-					state="progress"
+					state="complete"
 				/>
 			))}
 		</div>
--- src/components/form/select.tsx
+++ src/components/form/select.tsx
@@ -3,19 +3,22 @@
 import { ReactNode } from 'react'
 import { Check, ChevronDown } from 'lucide-react'
 import * as Select from '@radix-ui/react-select'
+import { twMerge } from 'tailwind-merge'
 
 type SelectItemProps = Select.SelectItemProps
 
 export function Item({ children, ...props }: SelectItemProps) {
 	return (
 		<Select.Item
-			className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50"
+			className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50 dark:data-[highlighted]:bg-zinc-700"
 			{...props}
 		>
-			<Select.ItemText className="text-black">{children}</Select.ItemText>
+			<Select.ItemText asChild>
+				<span className="text-zinc-950 dark:text-zinc-100">{children}</span>
+			</Select.ItemText>
 
 			<Select.ItemIndicator>
-				<Check className="h-4 w-4 text-violet-500" />
+				<Check className="h-4 w-4 text-violet-500 dark:text-violet-300" />
 			</Select.ItemIndicator>
 		</Select.Item>
 	)
@@ -31,7 +34,11 @@ export function Root({ children, id, placeholder, ...props }: SelectProps) {
 		<Select.Root {...props}>
 			<Select.Trigger
 				id={id}
-				className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600"
+				className={twMerge(
+					'flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none data-[placeholder]:text-zinc-600',
+					'focus:border-violet-300 focus:ring-4 focus:ring-violet-100',
+					'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20 dark:data-[placeholder]:text-zinc-400',
+				)}
 			>
 				<Select.Value className="text-black" placeholder={placeholder} />
 				<Select.Icon>
@@ -41,7 +48,10 @@ export function Root({ children, id, placeholder, ...props }: SelectProps) {
 
 			<Select.Portal>
 				<Select.Content
-					className="animate-slide-down-and-fade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+					className={twMerge(
+						'z-10 w-[--radix-select-trigger-width] animate-slide-down-and-fade overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm',
+						'dark:border-zinc-700 dark:bg-zinc-800',
+					)}
 					position="popper"
 					side="bottom"
 					sideOffset={8}
